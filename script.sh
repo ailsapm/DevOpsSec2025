da00d76589e8bc96c1ad74d4550607b6f53b5f44
Ailsa Prideaux-Mooney <x23283700@student.ncirl.ie>
2025-02-05 00:14:14 +0000

    second commit

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#leave an empty line for readability
+
+#create folders fol_1 and fol_2
+mkdir fol_1 fol_2
+
+#update to create files in fol_1
+touch fol_1/1_1.txt fol_1/1_2.txt fol_1/1_3.txt
+
+#update to create files in fol_2
+touch fol_2/2_1.txt fol_2/2_2.txt fol_2/2_3.txt
