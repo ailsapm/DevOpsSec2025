a0cf700d30a2787d4a01ead9903a49d09e5df0c8
Ailsa Prideaux-Mooney <x23283700@student.ncirl.ie>
2025-02-05 02:22:14 +0000

    changing permissions and adding readme file, also deleting and adding folders back in - last commit didn't take

--- script.sh
+++ script.sh
@@ -9,3 +9,16 @@ touch fol_1/1_1.txt fol_1/1_2.txt fol_1/1_3.txt
 
 #update to create files in fol_2
 touch fol_2/2_1.txt fol_2/2_2.txt fol_2/2_3.txt
+
+rm -r fol_1 fol_2
+
+touch fol_1/1_1.txt fol_1/1_2.txt fol_1/1_3.txt
+touch fol_2/2_1.txt fol_2/2_2.txt fol_2/2_3.txt
+
+#command to update permissions to read/write only for owner - if chmod worked in windows
+chmod 600 fol_1/1_1.txt fol_1/1_3.txt 
+chmod 600 fol_2/2_1.txt fol_2/2_3.txt 
+
+#command to update permissions to read/write.execute for all - if chmod worked in windows
+chmod 777 fol_1/1_2.txt
+chmod 777 fol_2/2_2.txt
